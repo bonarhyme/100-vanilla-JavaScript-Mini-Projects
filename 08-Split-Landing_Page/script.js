4c565607fd7bf77c519cfca0f2ad0b0cb107e325
bonarhyme <bonarhyme@gmail.com>
2021-04-28 21:04:12 +0100

    Added a split landing page

---
+++ 08-Split-Landing_Page/script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container")
+const right = document.querySelector(".right")
+const left = document.querySelector(".left")
+
+left.addEventListener("mouseenter", () => container.classList.add("hover-left"))
+left.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-left")
+)
+
+right.addEventListener("mouseenter", () =>
+  container.classList.add("hover-right")
+)
+right.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-right")
+)
